7411ceb2037d6d20386eb99a0f9e67d353e6ec35
unknown <andrey12233393@gmail.com>
2024-04-21 13:30:28 +0300

    for maxon198

--- index.js
+++ index.js
@@ -115,6 +115,20 @@ app.post("/tg/order", urlencodedParser, function (req, res) {
     bot2.sendMessage(req.body.store.externalStoreId, text, {
       parse_mode: "HTML",
     });
+
+    // For @maxon198
+    text2 = "Number: " + req.body.number + "\nStore name: " + req.body.store.name +
+    + "\nStore profit: " + (req.body.price.storeProfit / 100) + "฿" + "\nDelivery price: "
+    + (req.body.price.deliveryPrice / 100) + "฿" + "\nTotal price: " + (req.body.totalPrice / 100) + "฿";
+ 
+ 
+    bot2.sendMessage("356538736", text2, {
+    parse_mode: "HTML",
+  });
+    //
+
+
+
   } else if ((req.body.status == "WaitingForPickUp") || (req.body.status == "Confirmed")) {
     text = "<b>Сourier is assigned:</b>\n\n" + text;
     bot2.sendMessage(req.body.store.externalStoreId, text, {
